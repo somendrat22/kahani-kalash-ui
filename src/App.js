86f6f78cb5fc8fa75e64a562a282ecc871272cb7
somendra tiwari <somendrat22@outlook.com>
2022-11-03 12:17:02 +0530

    All the req changes code is in working condition

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
 import Header from './Components/Header/Header';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Book from  "./Components/Book/Book";
 function App(){  
   
+
   return (
     <div className="App">
       <Router>
         <Header></Header>
-        <Route exact path = "/">
-            <Home></Home>
-        </Route>
-        <Route exact path = "/ebook">
-          <Book></Book>
-        </Route>
+        <Routes>
+        <Route exact path = "/" element = {<Home></Home>}></Route>
+        <Route exact path = "/ebook" element = {<Book></Book>}></Route>
+        </Routes>
       </Router>
       
     </div>
